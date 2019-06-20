adcce9b20b6118a5b6227226c9df6600dcbccb08
juvenile <1171313930@qq.com>
2019-06-20 20:37:03 +0800

    提交chart

---
+++ package.sh
@@ -0,0 +1,5 @@
+tar zcvf mongodb-0.19.0.tgz mongodb
+tar zcvf fengshunli-mongodb-0.19.0.tgz fengshunli-mongodb
+
+tar zcvf nginx-0.5.0.tgz nginx
+tar zcvf fengshunli-nginx-0.5.0.tgz fengshunli-nginx
